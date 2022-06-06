18f926c2deb6a97dab52a975fd4ba598c1b15dcd
Remag29 <remag29@outlook.com>
2022-06-06 18:31:43 +0200

    Correct file

--- Tools.BaseModuleGenerator.psm1
+++ Tools.BaseModuleGenerator.psm1
@@ -2,12 +2,6 @@
 $script:ModulePath = Split-Path -Parent $PSCommandPath
 
 # Loading Private methods
-# Get-ChildItem -Path "$script:ModulePath\Enum" -Filter "*.ps1" | Where-Object { . $_.FullName }
-
-# Loading Dictionary
-Get-ChildItem -Path "$script:ModulePath\Dictionary" -Filter "*.ps1" | Where-Object { . $_.FullName }
-
-# Loading Private methods
 Get-ChildItem -Path "$script:ModulePath\Private" -Filter "*.ps1" | Where-Object { . $_.FullName }
 
 # Loading Public methods
