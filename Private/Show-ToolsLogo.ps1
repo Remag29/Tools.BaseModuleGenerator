94fa0dd38e389fec9efb3e6c66f0443e0001b11f
Remag29 <tristan.lucas.29@orange.fr>
2022-10-09 18:22:44 +0200

    Add function to display the module logo on the console

---
+++ Private/Show-ToolsLogo.ps1
@@ -0,0 +1,15 @@
+
+function Show-ToolsLogo {
+    
+    Write-Host ""
+    Write-Host "  ______            __  "  
+    Write-Host " /_  __/___  ____  / /____"
+    Write-Host "  / / / __ \/ __ \/ / ___/"
+    Write-Host " / / / /_/ / /_/ / (__  ) "
+    Write-Host "/_/  \____/\____/_/____(_)"
+    Write-Host ""
+    Write-Host "------------------------------"
+    Write-Host "Base Module Generator"
+    Write-Host "Version : $($MyInvocation.MyCommand.Module.Version)"                     
+    Write-Host ""
+}
