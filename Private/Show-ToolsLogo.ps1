ab1059a9372af94642602056ce7c1b4da333b7a6
Remag29 <tristan.lucas.29@orange.fr>
2022-10-09 18:27:51 +0200

    Added foreground color on logo

--- Private/Show-ToolsLogo.ps1
+++ Private/Show-ToolsLogo.ps1
@@ -2,14 +2,14 @@
 function Show-ToolsLogo {
     
     Write-Host ""
-    Write-Host "  ______            __  "  
-    Write-Host " /_  __/___  ____  / /____"
-    Write-Host "  / / / __ \/ __ \/ / ___/"
-    Write-Host " / / / /_/ / /_/ / (__  ) "
-    Write-Host "/_/  \____/\____/_/____(_)"
+    Write-Host "  ______            __  "   -ForegroundColor Cyan
+    Write-Host " /_  __/___  ____  / /____" -ForegroundColor Cyan
+    Write-Host "  / / / __ \/ __ \/ / ___/" -ForegroundColor Cyan
+    Write-Host " / / / /_/ / /_/ / (__  ) " -ForegroundColor Cyan
+    Write-Host "/_/  \____/\____/_/____(_)" -ForegroundColor Cyan
     Write-Host ""
-    Write-Host "------------------------------"
-    Write-Host "Base Module Generator"
-    Write-Host "Version : $($MyInvocation.MyCommand.Module.Version)"                     
+    Write-Host "------------------------------" -ForegroundColor Cyan
+    Write-Host "Base Module Generator" -ForegroundColor Cyan
+    Write-Host "Version : $($MyInvocation.MyCommand.Module.Version)" -ForegroundColor Cyan              
     Write-Host ""
 }
